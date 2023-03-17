20f6b49d633170b1e00c980a16c30065d26c65f1
Yasmin <ysnsaless@gmail.com>
2023-03-16 21:59:56 -0300

    Tela historico

--- src/pages/HistoricPage/HistoricPage.js
+++ src/pages/HistoricPage/HistoricPage.js
@@ -1,5 +1,37 @@
 import styled from "styled-components"
+import TopoMenu from "../TopoMenu"
 
 export default function HistoricPage(){
-    return("oi")
+    return(<>
+        <TopoMenu />
+        <PageContainer>
+            <h1>Histórico</h1>
+            <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+        </PageContainer>
+        </>
+    )
 }
+
+const PageContainer = styled.div` 
+justify-content: center;
+padding: 98px 18px;
+h1{
+    font-family: 'Lexend Deca', sans-serif;
+font-style: normal;
+font-weight: 400;
+font-size: 22.976px;
+line-height: 29px;
+margin-bottom: 17px;
+
+
+color: #126BA5;
+}
+p{
+    font-family: 'Lexend Deca', sans-serif;
+font-style: normal;
+font-weight: 400;
+font-size: 17.976px;
+line-height: 22px;
+
+color: #666666;
+}`
--- src/pages/TodayPage/TodayPage.js
+++ src/pages/TodayPage/TodayPage.js
@@ -1,7 +1,8 @@
 import styled from "styled-components"
+import TopoMenu from "../TopoMenu"
 
 export default function TodayPage(){
     return(
-        <>Oi</>
+        <TopoMenu />
     )
 }
